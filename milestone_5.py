4e916e4dec3be098e87fe5389500b88147a7e5f3
Lewis Crowther <l3wisdc@hotmail.com>
2023-07-28 14:29:42 +0100

    Added docstring and comments

--- milestone_5.py
+++ milestone_5.py
@@ -1,25 +1,51 @@
 import random
 
 class Hangman:
+
+    '''
+    Hangman Game Class
+
+    This class represents a simple Hangman game. The game randomly selects a word from a given list of words,
+    and the player has to guess the letters in the word within a limited number of attempts (lives). The game
+    ends when the player either guesses all the letters correctly or runs out of lives.
+
+    Attributes:
+        word_list (list): A list of words from which the game randomly selects the secret word.
+        num_lives (int): The number of lives the player starts with (default is 5).
+        list_of_guesses (list): A list of letters guessed by the player.
+        word (str): The randomly selected word to be guessed.
+        word_guessed (list): A list representing the current status of the word being guessed, with underscores
+                            for unguessed letters and correct letters revealed.
+        num_letters (int): The number of unique letters in the word that have not been guessed yet.
+
+    Methods:
+        pick_random_word(): Selects a random word from the word_list attribute.
+        check_guess(guess): Checks if the guessed letter is in the secret word and updates the game status.
+        ask_for_input(): Prompts the player to guess a letter and manages player input and game outcomes.
+    '''
+
+
     def __init__(self, word_list, num_lives = 5):
-        # List of words
+        # list of words
         self.word_list = word_list
-        # Number of lives the player has at the start of the game
+        # number of lives the player has at the start of the game
         self.num_lives = num_lives
-        # A list of the guesses that have already been tried
+        # a list of the guesses that have already been tried
         self.list_of_guesses = []
-        # Select a random word
+        # select a random word
         self.word = self.pick_random_word()
-        # Blank list of underscores to store and display the guessed letters
+        # blank list of underscores to store and display the guessed letters
         self.word_guessed = ["_"] * len(self.word)
-        # Number of unique letters that have not been guessed yet
+        # number of unique letters that have not been guessed yet
         self.num_letters = len(set(self.word))
 
     def pick_random_word(self):
         return random.choice(self.word_list)
 
     def check_guess(self, guess):
+        # convert the guessed letter to lowercase
         guess = guess.lower()
+        # check if the guessed letter is in the secret word
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             # loop through the letters in self.word
@@ -28,29 +54,41 @@ class Hangman:
                 if letter == guess:
                     # set the underscore at that position to the correct letter
                     self.word_guessed[i] = letter
+            # print current status of the secret word
             print(self.word_guessed)
+            # decrement number of letters left to guess
             self.num_letters -= 1
+        # if the guess letter is incorrect
         else:
+            # decrement number of lives
             self.num_lives -= 1
             print(f"Sorry. {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left")
 
     def ask_for_input(self):
+        # underscore representation of the secret word
         print(self.word_guessed)
         while True:
-            guess = input("Guess a letter: ")            
+            # prompt the player to guess a letter
+            guess = input("Guess a letter: ")
+            # check if the guess is a single letter         
             if len(guess) != 1 or not(guess.isalpha()):
                 print("Invalid letter. Please, enter a single alphabetical character.")
+            # check if letter has already been guessed
             elif guess in self.list_of_guesses:
                 print("You already tried that letter")
             else:
-                self.check_guess(guess) # Calls check_guess function
+                # calls check_guess function
+                self.check_guess(guess)
+                # add the guessed letter to the list of guesses
                 self.list_of_guesses.append(guess)
 
+                # when number of lives reach zero, the player loses
                 if self.num_lives == 0:
                     print("You lost!")
                     return False
                 
+                # when the player has guessed all the letters, the player wins
                 elif self.num_letters == 0:
                     print(f"{''.join(self.word_guessed)} is correct!")
                     print("Congradulations. You won the game!")
